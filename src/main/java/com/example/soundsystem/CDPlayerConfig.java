a20101e4101bb3bf7307b6a1c4dd45e3a0b257f9
think12381 <think123811@gmail.com>
2019-06-03 15:51:58 +0800

    add test module and test component and autowire concept

--- src/main/java/com/example/soundsystem/CDPlayerConfig.java
+++ src/main/java/com/example/soundsystem/CDPlayerConfig.java
@@ -1,10 +1,12 @@
 package com.example.soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ComponentScan
+//@ComponentScan // using java configuration for deploy so mark @component
 public class CDPlayerConfig {
+
 }
---
+++ src/main/java/com/example/soundsystem/MediaPlayer.java
@@ -0,0 +1,6 @@
+package com.example.soundsystem;
+
+public interface MediaPlayer {
+    void play();
+
+}
